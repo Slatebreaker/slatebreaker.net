9da268d2722dc5f7dc26cc2db44a69dd745c486f
Slatebreaker <slatebreaker@pm.me>
2025-09-20 16:56:42 +1000

    Add pubkey page to repo

---
+++ pubkey/index.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<div style="width: fit-content; text-align: center;">
+  <a href=slatebreaker_pubkey.asc> Download public key (.asc) </a><br>
+
+  <pre style="font-family: monospace;">
+    <?=file_get_contents("slatebreaker_pubkey.asc")?>
+  </pre>
+</div>
